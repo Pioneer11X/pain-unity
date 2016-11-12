9d2bbe928ee2c96c6f7364fa83de28c5e1e32946
Sravan Kumar Karuturi (RIT Student) <skk7174@ad.rit.edu>
2016-11-11 21:29:53 -0500

    Character Moving Sideways

---
+++ pain-unity/Pain-Unity/Assets/Block.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        fade();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnMouseDown()
+    {
+        fade();
+    }
+
+    void fade()
+    {
+        Color tmp = GetComponent<SpriteRenderer>().color;
+        tmp.a = tmp.a - (float)0.1;
+        GetComponent<SpriteRenderer>().color = tmp;
+    }
+}
---
+++ pain-unity/Pain-Unity/Assets/Movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public float Speed = 10f;
+    private float movex = 0f;
+    private float movey = 0f;
+    private Rigidbody2D wololo;
+
+    // Use this for initialization
+    void Start () {
+         wololo = GetComponent<Rigidbody2D>();
+        //Debug.Log("Kappa");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+      
+    }
+
+    void FixedUpdate()
+    {
+        movex = Input.GetAxis("Horizontal");
+        //movey = Input.GetAxis("Vertical");
+        wololo.velocity = new Vector2(movex * Speed, wololo.velocity.y);
+
+    }
+}
